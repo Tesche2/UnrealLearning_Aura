d34a1149c880ee3900586611e9b2c0b8e7991ada
Tesche2 <nikolastesche@gmail.com>
2025-02-05 18:57:26 -0300

    Aura User Widget and Widget Controller

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// CopyrightTesche
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
